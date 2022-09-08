f7e3dbcb96511ac98366517c8c4e45eb87ee1a2f
6ase <serghome06@mail.ru>
2022-09-08 16:12:02 +0300

    cnav+video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Video from './Video';
 import Login from './Login';
-
 import Navbar from './Navbar';
+import Registration from './Registration';
 
 export default function App({ userSession }) {
   const [authState, setAuthState] = useState(userSession || null);
@@ -10,6 +11,7 @@ export default function App({ userSession }) {
 
     <div className="container">
       <div className="row">
+        <Video />
         <Navbar authState={authState} setAuthState={setAuthState} />
         <Routes>
 
